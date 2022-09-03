9ff99c966c577d0345c76bf820aa0356511dc7b5
legrs <73222814+legrs@users.noreply.github.com>
2022-09-03 19:30:47 +0900

    Create js.js

---
+++ js.js
@@ -0,0 +1,10 @@
+btn1 = document.querySelector("#btnrlo");
+btn2 = document.querySelector("#btnlro");
+
+btn1.addEventListner('click',()=>{
+  navigator.clipboard.writeText('‮‮');
+}
+
+btn1.addEventListner('click',()=>{
+  navigator.clipboard.writeText('‭');
+}
