40f0c5fc3c07e384f45d7eaa4932a3878fae75f9
legrs <73222814+legrs@users.noreply.github.com>
2022-09-03 19:39:13 +0900

    Update js.js

--- js.js
+++ js.js
@@ -1,8 +1,9 @@
 let btn1 = document.querySelector("#btnrlo");
 let btn2 = document.querySelector("#btnlro");
+let rlo = "‮‮";
 
 btn1.addEventListner('click',()=>{
-  navigator.clipboard.writeText('‮‮');
+  navigator.clipboard.writeText(rlo);
 }
 
 btn1.addEventListner('click',()=>{
